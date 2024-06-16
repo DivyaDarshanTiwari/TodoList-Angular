f1144e32ac17dc59a275eeb868e69d188d300eec
DivyaDarshanTiwari <davidrocco400@outlook.com>
2024-06-16 22:36:56 +0530

    addign part done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgFor } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'TodoList';
-}
+
+  list:{
+    id: number,
+    name: string
+    priority: string,
+    date: string
+  }[]=[]
+
+  addition(work:string,prior:string,date1:string)
+  {
+    const date= new Date(date1);
+    console.log(work,prior,date1);
+    this.list.push({id:this.list.length,name: work, priority:prior, date: date1})
+    console.log(this.list);
+  }
+  }
